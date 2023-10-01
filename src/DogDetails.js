832049bfc3d2fa5c5de75e59078498786684a144
Michael <michaellngriffin@gmail.com>
2023-10-01 19:09:47 -0400

    start of work on dogfinder

---
+++ src/DogDetails.js
@@ -0,0 +1,43 @@
+import { useParams } from "react-router-dom";
+
+// {
+//   "name": "Whiskey",
+//     "age": 5,
+//       "src": "whiskey",
+//         "facts": [
+//           "Whiskey loves eating popcorn.",
+//           "Whiskey is a terrible guard dog.",
+//           "Whiskey wants to cuddle with you!"
+//         ];
+// }
+
+/**
+ * It's either, getting all of the above from server?
+ * Or getting it passed in as props?
+ */
+function DogDetails() {
+
+  const params = useParams();
+  const {name: dogName} = params;
+
+  function find(dogName){
+    //search through imported JSON?
+  }
+
+
+  function displayFacts(facts){
+    return facts.map((fact, ind) => {
+      <p key={ind}>{fact}</p>
+    })
+  }
+
+  return (
+    <div className="DogDetails">
+      <p>This dog's name is {name}, who is age {age}.</p>
+      <p>Here are some facts about {name}</p>
+      {displayFacts()}
+    </div>
+  );
+}
+
+export default DogDetails;
---
+++ src/Nav.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+function Nav({names}) {
+  //make a list from names.
+
+  // function makeLinks(){
+  //   return names.map(name => {
+  //     return <Link to={`/dogs${name}`}>{name}</Link>;
+  //   })
+  // }
+
+  // return (
+  //   <nav className="Nav">
+  //     {makeLinks()}
+  //   </nav>
+  // )
+}
+
+
+export default Nav;
